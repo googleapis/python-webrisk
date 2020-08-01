d73babcf7ec60a160473cb979c61aea16b466bc3
yoshi-automation <yoshi-automation@google.com>
2020-08-01 14:09:27 -0700

    Remove experimental warning for ordering keys properties.
    PiperOrigin-RevId: 323803770
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Jul 29 09:31:38 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 706053bbfb3f16ef752e513c2898a1f26cdd8e41
    Source-Link: https://github.com/googleapis/googleapis/commit/706053bbfb3f16ef752e513c2898a1f26cdd8e41

--- google/cloud/webrisk_v1beta1/services/web_risk_service_v1_beta1/transports/base.py
+++ google/cloud/webrisk_v1beta1/services/web_risk_service_v1_beta1/transports/base.py
@@ -104,7 +104,7 @@ class WebRiskServiceV1Beta1Transport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                     ),
                 ),
                 default_timeout=600.0,
@@ -117,7 +117,7 @@ class WebRiskServiceV1Beta1Transport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                     ),
                 ),
                 default_timeout=600.0,
@@ -130,7 +130,7 @@ class WebRiskServiceV1Beta1Transport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
+                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
                     ),
                 ),
                 default_timeout=600.0,
