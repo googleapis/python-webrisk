095a84d6234ac1308f9ac978fccdc27680a69472
yoshi-automation <yoshi-automation@google.com>
2020-12-23 12:11:00 -0800

    chore: use Python microgenerator rules for aiplatform
    PiperOrigin-RevId: 333302099
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Sep 23 08:58:22 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: acd4aabf1c1e3e5bf7db0e81f323003fb317fdaa
    Source-Link: https://github.com/googleapis/googleapis/commit/acd4aabf1c1e3e5bf7db0e81f323003fb317fdaa

--- google/cloud/webrisk_v1/services/web_risk_service/async_client.py
+++ google/cloud/webrisk_v1/services/web_risk_service/async_client.py
@@ -191,7 +191,7 @@ class WebRiskServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                 ),
             ),
             default_timeout=600.0,
@@ -277,7 +277,7 @@ class WebRiskServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                 ),
             ),
             default_timeout=600.0,
@@ -365,7 +365,7 @@ class WebRiskServiceAsyncClient:
                 maximum=60.0,
                 multiplier=1.3,
                 predicate=retries.if_exception_type(
-                    exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                    exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                 ),
             ),
             default_timeout=600.0,
--- google/cloud/webrisk_v1/services/web_risk_service/transports/base.py
+++ google/cloud/webrisk_v1/services/web_risk_service/transports/base.py
@@ -111,7 +111,7 @@ class WebRiskServiceTransport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                     ),
                 ),
                 default_timeout=600.0,
@@ -124,7 +124,7 @@ class WebRiskServiceTransport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                     ),
                 ),
                 default_timeout=600.0,
@@ -137,7 +137,7 @@ class WebRiskServiceTransport(abc.ABC):
                     maximum=60.0,
                     multiplier=1.3,
                     predicate=retries.if_exception_type(
-                        exceptions.DeadlineExceeded, exceptions.ServiceUnavailable,
+                        exceptions.ServiceUnavailable, exceptions.DeadlineExceeded,
                     ),
                 ),
                 default_timeout=600.0,
